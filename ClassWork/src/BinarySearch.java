d95ecba0053e74a2dfce438651915fc5f25e8ee0
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-11-15 13:17:13 -0500

    junk

--- ClassWork/src/BinarySearch.java
+++ ClassWork/src/BinarySearch.java
@@ -6,14 +6,63 @@ public class BinarySearch {
 		int[] test1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		int[] test2 = {0, 4, 5, 6, 12, 15, 17, 23, 29};
 		
-		//System.out.println(binarySearch(test1, 3, 0, test1.length));
-		//System.out.println(binarySearch(test2, 23, 0, test2.length));
-		//System.out.println(binarySearch(test1, 1, 0, test1.length));
+		for(int i = -1; i < test1.length+1; i++)
+		{
+			System.out.print(binarySearch(test1, i, 0, test1.length) + "  ");
+		}
+		System.out.println();
+		for(int i = -1; i < test2.length+1; i++)
+		{
+			System.out.print(binarySearch(test2, i, 0, test2.length) + "  ");
+		}
+		System.out.println();
+		
+		System.out.println((7+4)/2);
+		
+		/*
+		System.out.println(binarySearch(test1, 3, 0, test1.length));
+		System.out.println(binarySearch(test2, 23, 0, test2.length));
+		System.out.println(binarySearch(test1, 1, 0, test1.length));
 		System.out.println(binarySearch(test2, 25, 0, test2.length));
-		//System.out.println(binarySearch(test2, 29, 0, test2.length));
+		System.out.println(binarySearch(test2, 29, 0, test2.length));
+		 */
 
 	}
 	
+	public static int binarySearch(int[] nums, int query, int first, int last)
+	{
+					
+		
+		if(first < last)
+		{
+			int index = (first + last)/2;
+			int guess = nums[index];
+			//System.out.println(index);
+			
+			if(guess == query)
+			{
+				return index;
+			}
+
+			if(guess > query)
+			{
+				return binarySearch(nums, query, first, index);
+			}
+			
+			if(guess < query)
+			{
+				return binarySearch(nums, query, index+1, last);
+			}
+			
+			
+		}
+		
+		return -1;
+	}
+
+}
+
+/* Old Solution
 	public static int binarySearch(int[] nums, int query, int first, int last)
 	{
 		if(last >= first)
@@ -48,3 +97,6 @@ public class BinarySearch {
 	}
 
 }
+
+*/
+
